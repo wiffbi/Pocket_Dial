3dd5077588b65adedd8d4b3b006c92f57f9a9950
Richard Schreiber <wiffbi@gmail.com>
2014-07-06 13:28:28 +0200

    Added support for Live 9

--- __init__.py
+++ __init__.py
@@ -1,4 +1,10 @@
 #__init__.py
-from PocketDial import PocketDial
+import Live
+
+if Live.Application.get_application().get_major_version() == 9:
+    from PocketDial9 import PocketDial
+else:
+    from PocketDial8 import PocketDial
+
 def create_instance(c_instance):
     return PocketDial(c_instance)
